7734dd5b263d9185eab3df547dd9ea0fd7ba3dd0
Anuu <110338926+Manishgupta64047@users.noreply.github.com>
2023-05-19 14:13:12 +0545

    Update and rename vars (2).py to vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,26 +11,26 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID','17954446'))
     API_HASH = str(getenv('API_HASH','b9d62d60af7542ad41274a47a5c9f369'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN','5959633763:AAES9rWaQDsZEBT4ACWzxGqubxwQKjCMA-0'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN','6211197496:AAHoh-O4x28K-sEVv8ZTg9xhYZ6zabrnnQE'))
     name = str(getenv('SESSION_NAME', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL','-100920432353'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL','-920432353'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', ''))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1413071149").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
+    NO_PORT = bool(getenv('NO_PORT', True))
+    APP_NAME = True
     OWNER_USERNAME = str(getenv('OWNER_USERNAME','sigma_male_007'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME','file-2-link-pyro-maxx'))
+        APP_NAME = str(getenv('APP_NAME','file-2-link-pyro-maxed'))
     
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.onrender.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
+    HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
